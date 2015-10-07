da1c6d0e18ebb0fc63d60a190ba4d1b7688fec2f
shivam kaushik <shivamkaushik@shivams-macbook-air.local>
2015-10-07 16:33:18 +0530

    Initial Commit

---
+++ rpgoop-exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpgoop-exercise
+//
+//  Created by shivam kaushik on 07/10/15.
+//  Copyright © 2015 shivam kaushik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
